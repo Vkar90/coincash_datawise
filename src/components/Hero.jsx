1354ff681610e97b5fe1edf32ebfad86746e6d4e
Kardamitsis Evan <kardamitsis.e@gmail.com>
2023-10-10 16:31:10 +0300

    minor fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import "../styles/hero.scss";
 import iphone from "../assets/iphone.svg";
