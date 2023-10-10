d2917b5d0a8458164fdc9e32ff466015a7dfb60f
Kardamitsis Evan <kardamitsis.e@gmail.com>
2023-10-10 15:24:15 +0300

    adjust hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import "../styles/hero.scss";
 import iphone from "../assets/iphone.svg";
 import mobileIphone from "../assets/mobile_hero_image.svg";
@@ -7,10 +8,18 @@ import mobileIphone from "../assets/mobile_hero_image.svg";
 import Header from "./Header";
 
 const Hero = () => {
-  const slideUpFadeIn = {
-    hidden: { opacity: 0, y: "100vh" },
-    visible: { opacity: 1, y: 0 },
+  const fadeIn = {
+    hidden: { opacity: 0, duration: 1 },
+    visible: { opacity: 1 },
   };
+
+  // state and variables for color changing text
+  const text = "COINCASH";
+  const [colors, setColors] = useState(Array(text.length).fill("black"));
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0,
+  });
 
   // state to hold the button text, hero image and window width
   const [buttonText, setButtonText] = useState("Download now for free");
@@ -31,16 +40,53 @@ const Hero = () => {
     setHeroImage(windowWidth <= 900 ? mobileIphone : iphone);
   }, [windowWidth]);
 
+  const runEffect = () => {
+    const timeouts = [];
+    for (let i = 0; i < text.length; i++) {
+      const timeout = setTimeout(() => {
+        setColors((prevColors) => {
+          // Copy the previous colors array
+          const newColors = [...prevColors];
+          // Set the color of the character at index i to primary
+          newColors[i] = "#0066ff";
+          // Return the updated colors array
+          return newColors;
+        });
+      }, i * 200); // Adjust delay as needed
+      timeouts.push(timeout);
+    }
+    return () => timeouts.forEach(clearTimeout);
+  };
+
+  useEffect(() => {
+    if (inView) {
+      // Reset colors to initial state
+      setColors(Array(text.length).fill("black"));
+      // Run the effect
+      runEffect();
+    }
+  }, [inView]);
+
   return (
-    <section className="hero">
+    <section className="hero" ref={ref}>
       <div className="hero-container">
         <Header />
         <div className="hero-content-container">
           <div className="hero-cta-container">
             <div className="hero-text-container">
               <h1 className="hero-cta">
-                You can buy anything with
-                <span className="brand-name"> COINCASH</span>
+                You can buy anything with{" "}
+                {text.split("").map((letter, index) => (
+                  <span
+                    key={index}
+                    className={`letter ${
+                      colors.includes(index) ? "letter-blue" : ""
+                    }`}
+                    style={{ color: colors[index] }}
+                  >
+                    {letter}
+                  </span>
+                ))}
               </h1>
               <h2 className="hero-cta-description">
                 The easiest way to use your cryptocurrencies for everyday
@@ -57,7 +103,7 @@ const Hero = () => {
               alt="coincash feature image"
               initial="hidden"
               animate="visible"
-              variants={slideUpFadeIn}
+              variants={fadeIn}
               transition={{ duration: 1, ease: "easeInOut" }}
             />
           </div>
